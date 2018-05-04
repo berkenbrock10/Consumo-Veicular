1c86225699136cb7f81aef8245028400062dddfd
Christian <christian.berkenbrock@chaordicsystems.com>
2018-05-04 16:52:06 -0300

    final

---
+++ src/Viagem.java
@@ -0,0 +1,27 @@
+
+public class Viagem {
+
+	private float kmLitro;
+	private float distancia;
+	
+	Viagem(float kmLitro, float distancia) {
+		this.kmLitro = kmLitro;
+		this.distancia = distancia;
+	}
+
+	public float getKmLitro() {
+		return kmLitro;
+	}
+
+	public void setKmLitro(float kmLitro) {
+		this.kmLitro = kmLitro;
+	}
+
+	public float getDistancia() {
+		return distancia;
+	}
+
+	public void setDistancia(float distancia) {
+		this.distancia = distancia;
+	}
+}
